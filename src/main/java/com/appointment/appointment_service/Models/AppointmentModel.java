0afc35c2b3ced8b2fd195126482d39b9370b5f9d
Ubed-pathan <ubedpathan818@gmail.com>
2025-07-24 15:03:53 +0530

    model designed

--- src/main/java/com/appointment/appointment_service/Models/AppointmentModel.java
+++ src/main/java/com/appointment/appointment_service/Models/AppointmentModel.java
@@ -1,7 +1,63 @@
 package com.appointment.appointment_service.Models;
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
 
 @Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class AppointmentModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String appointmentId;
+
+    @Column(
+            nullable = false,
+            updatable = false
+    )
+    private LocalDateTime appointmentTime;
+
+    @Column(
+            nullable = false,
+            updatable = false
+    )
+    private String userId;
+
+    @Column(
+            nullable = false,
+            updatable = false
+    )
+    private String doctorId;
+
+    @Column(
+                    nullable = false,
+                    length = 250
+    )
+    private String reason;
+
+    @Enumerated(EnumType.STRING)
+    private AppointmentStatus status = AppointmentStatus.SCHEDULED;
+
+    public enum AppointmentStatus {
+        SCHEDULED,
+        CANCELLED,
+        COMPLETED,
+    }
+
+    @Column(
+            nullable = false,
+            updatable = false
+    )
+    private LocalDateTime createdAt;
+
+    @PrePersist
+    protected void onCreate() {
+        createdAt = LocalDateTime.now();
+    }
 }
--- src/main/java/com/appointment/appointment_service/Repositories/AppointmentRepository.java
+++ src/main/java/com/appointment/appointment_service/Repositories/AppointmentRepository.java
@@ -3,5 +3,30 @@ package com.appointment.appointment_service.Repositories;
 import com.appointment.appointment_service.Models.AppointmentModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 public interface AppointmentRepository extends JpaRepository<AppointmentModel, String> {
+    // Find all appointments for a specific patient
+    List<AppointmentModel> findByPatientId(String patientId);
+
+    // Find all appointments for a specific doctor
+    List<AppointmentModel> findByDoctorId(String doctorId);
+
+
+    // Find appointments based on date/time string if you format it properly
+    List<AppointmentModel> findByAppointmentTime(LocalDateTime appointmentTime);
+
+    boolean existsByDoctorIdAndAppointmentTime(String doctorId, LocalDateTime appointmentTime);
+
+    boolean existsByUserIdAndAppointmentTime(String userId, LocalDateTime appointmentTime);
+
+    boolean existsByUserIdAndDoctorIdAndAppointmentTimeBetween(
+            String userId,
+            String doctorId,
+            LocalDateTime startOfDay,
+            LocalDateTime endOfDay
+    );
+
+
 }
