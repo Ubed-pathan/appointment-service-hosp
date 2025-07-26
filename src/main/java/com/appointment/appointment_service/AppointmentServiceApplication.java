0f4a8306aefe0f65f799ef3b4a3f01c905f0c2da
Ubed-pathan <ubedpathan818@gmail.com>
2025-07-26 13:20:41 +0530

    added context path

--- src/main/java/com/appointment/appointment_service/AppointmentServiceApplication.java
+++ src/main/java/com/appointment/appointment_service/AppointmentServiceApplication.java
@@ -2,8 +2,10 @@ package com.appointment.appointment_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 // from spring boot 3 and above @EnableEurekaClient is not needed
 public class AppointmentServiceApplication {
 
